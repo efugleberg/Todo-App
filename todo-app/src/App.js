3d73dd9e94e6aa6227ad842d37f5d5d4c86c898d
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-11-12 16:07:16 -0600

    through delete todos

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with spouse",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
+  // Toggle Completed
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
